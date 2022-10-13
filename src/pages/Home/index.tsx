d99d8ed6dae4dd8898560526c48e3009ea927405
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-13 14:59:51 +0300

    bring out

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {videoAPI} from "../../sevices/VideoService";
 
 const Index = () => {
+    const [limit, setLimit] = useState(100)
+    const {data: videos, error, isLoading} = videoAPI.useFetchAllPostsQuery(limit)
+
+
     return (
         <div>
-           Home
+            {isLoading && <h1>Loading...</h1>}
+            {error && <h1>Error when loading</h1>}
+            {videos && videos.map(videos =>
+                <div key={videos.title}>{videos.title}</div>
+            )}
         </div>
     );
 };
