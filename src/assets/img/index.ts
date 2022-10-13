b748f95e5759c42623e0cf94ed604fe18c5e4843
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-13 14:05:30 +0300

    Install images, Created Style

---
+++ src/assets/img/index.ts
@@ -0,0 +1,6 @@
+import Logo from "./logo.png";
+import LogoWhite from "./logo-white.png";
+import LogoIcon from "./youtubeLogo.png";
+import youtubeBanner from "./youtubeBanner.png";
+
+export {Logo, LogoWhite, LogoIcon, youtubeBanner}
