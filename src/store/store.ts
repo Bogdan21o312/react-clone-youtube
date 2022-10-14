a911757205ad8562af9abbf072e040594a079129
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-14 19:50:41 +0300

    Connected new API

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,24 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from './reducers/UserSlice'
-import {videoAPI} from "../sevices/VideoService";
-import {categoriesAPI} from "../sevices/CategoriesService";
+import {videoAPI} from "../sevices/VideoServices";
+import {categoriesAPI} from "../sevices/CategoriesServices";
+import {mainTabsAPI} from "../sevices/MainTabsServices";
+import {externalTabsAPI} from "../sevices/ExternalTabsServices";
+import {channelsAPI} from "../sevices/ChannelsServices";
+import {exploreAPI} from "../sevices/ExploreServices";
+import {moreYoutubesAPI} from "../sevices/MoreYoutubesServices";
+import {settingsAPI} from "../sevices/SettingsServices";
 
 const rootReducer = combineReducers({
     userReducer,
     [videoAPI.reducerPath]: videoAPI.reducer,
     [categoriesAPI.reducerPath]: categoriesAPI.reducer,
+    [mainTabsAPI.reducerPath]: mainTabsAPI.reducer,
+    [externalTabsAPI.reducerPath]: externalTabsAPI.reducer,
+    [channelsAPI.reducerPath]: channelsAPI.reducer,
+    [exploreAPI.reducerPath]: exploreAPI.reducer,
+    [moreYoutubesAPI.reducerPath]: moreYoutubesAPI.reducer,
+    [settingsAPI.reducerPath]: settingsAPI.reducer,
 })
 
 export const setupStore = () => {
