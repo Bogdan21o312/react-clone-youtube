9f534b7f9e689e2ecd85d00ea842c8fc6a59d22d
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-14 19:49:44 +0300

    Created MoreYoutubesServices.ts

---
+++ src/sevices/MoreYoutubesServices.ts
@@ -0,0 +1,43 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
+import {IChannels} from "../models/IChannels";
+
+export const moreYoutubesAPI = createApi({
+    reducerPath: 'moreYoutubesAPI',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3000'}),
+    tagTypes: ['MoreYoutubes'],
+    endpoints: (build) => ({
+        fetchAllPosts: build.query<IChannels[], number>({
+            query: (limit: number = 5) => ({
+                url: '/moreYoutubes',
+                params: {
+                    _limit: limit
+                }
+            }),
+            providesTags: result => ['MoreYoutubes']
+        }),
+        createPost: build.mutation<IChannels, IChannels>({
+            query: (post) => ({
+                url: '/moreYoutubes',
+                method: 'POST',
+                body: post
+            }),
+            invalidatesTags: ['MoreYoutubes']
+        }),
+        updatePost: build.mutation<IChannels, IChannels>({
+            query: (post) => ({
+                url: `/moreYoutubes/${post.id}`,
+                method: 'PUT',
+                body: post
+            }),
+            invalidatesTags: ['MoreYoutubes']
+        }),
+        deletePost: build.mutation<IChannels, IChannels>({
+            query: (post) => ({
+                url: `/moreYoutubes/${post.id}`,
+                method: 'DELETE',
+                body: post
+            }),
+            invalidatesTags: ['MoreYoutubes']
+        }),
+    })
+})
